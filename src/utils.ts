446ec6f0c964331299c5220a2bbfe63683808b15
tuphan-dn <tuphan@descartes.network>
2021-07-25 16:01:50 +0700

    merge math to util

---
+++ src/utils.ts
@@ -0,0 +1,144 @@
+import axios from 'axios'
+import * as BN from 'bn.js'
+import * as nacl from 'tweetnacl'
+import { LAMPORTS_PER_SOL } from '@solana/web3.js'
+import * as emoji from './data/emoji.json'
+
+const PRECISION = new BN('1000000000')
+
+const util = {
+  /**˝
+   * Basic transaction fee
+   */
+  BASIC_TX_FEE: 0.000005,
+
+  /**
+   * Total number of emoji for address mapping
+   */
+  TOTAL_EMOJI: emoji.length,
+
+  /**
+   * Sol decimals
+   */
+  LAMPORTS_PER_SOL: LAMPORTS_PER_SOL,
+
+  /**
+   * Map an address to an emoji
+   * @param seed maybe your address
+   * @returns an emoji
+   */
+  randEmoji: (seed: string) => {
+    const hash = nacl.hash(Buffer.from(seed))
+    const shortenedHash = Buffer.from(hash.subarray(0, 4)).toString('hex')
+    const index = parseInt(shortenedHash, 16)
+    return emoji[index % util.TOTAL_EMOJI]
+  },
+
+  /**
+   * Parse Coingecko
+   * @param ticket Coingecko ticket of the coin/token
+   * @returns icon, symcol, name, address, rank, price, priceChange (24h), totalVolume
+   */
+  parseCGK: async (ticket = '') => {
+    const {
+      data: {
+        image: { large, small, thumb },
+        symbol: refSymbol,
+        name,
+        platforms: { solana },
+        market_cap_rank: rank,
+        market_data: {
+          current_price: { usd: price },
+          total_volume: { usd: totalVolume },
+          price_change_percentage_24h: priceChange,
+        },
+      },
+    } = await axios({
+      method: 'get',
+      url: 'https://api.coingecko.com/api/v3/coins/' + ticket,
+    })
+    const icon = large || thumb || small
+    const symbol = refSymbol.toUpperCase()
+    const address = solana
+    return {
+      icon,
+      symbol,
+      name,
+      address,
+      rank,
+      price,
+      priceChange,
+      totalVolume,
+    }
+  },
+
+  /**
+   * Add decimals to the number
+   * @param a
+   * @param decimals
+   * @returns
+   */
+  decimalize: (a: string | number, decimals: number): BigInt => {
+    if (!a) return BigInt(0)
+    if (decimals <= 0 || decimals % 1 != 0) return BigInt(0)
+    const n = a.toString()
+    const m = n.split('.')
+    if (m.length > 2) throw new Error('Invalid number')
+    if (m.length == 1) return BigInt(a) * BigInt(10 ** decimals)
+    if (m[1].length >= decimals)
+      return BigInt(m[0] + m[1].substring(0, decimals))
+    else return BigInt(m[0] + m[1] + '0'.repeat(decimals - m[1].length))
+  },
+
+  /**
+   * Remove decimals from the number
+   * @param a
+   * @param decimals
+   * @returns
+   */
+  undecimalize: (a: BigInt, decimals: number) => {
+    if (decimals < 0 || decimals % 1 != 0)
+      throw new Error('decimals must be an integer greater than zero')
+    if (!a) return '0'
+    if (!decimals) return '1'
+    const n = a.toString()
+
+    let integer =
+      n.length > decimals ? n.substring(0, n.length - decimals) : '0'
+    let fraction: string | string[] = ''
+    if (n.length > decimals)
+      fraction = n.substring(n.length - decimals, n.length)
+    else if (n.length == decimals) fraction = n
+    else fraction = '0'.repeat(decimals - n.length) + n
+
+    fraction = fraction.split('')
+    while (fraction[fraction.length - 1] === '0') fraction.pop()
+    fraction = fraction.join('')
+    if (!fraction) return integer
+    return integer + '.' + fraction
+  },
+
+  /**
+   * Divide two numbers
+   * @param a
+   * @param b
+   * @returns
+   */
+  div: (a: string | number | BigInt, b: string | number | BigInt): number => {
+    if (!b) throw new Error('Cannot be divided by 0')
+    if (!a) return 0
+    const ba = new BN(a.toString())
+    const bb = new BN(b.toString())
+    const bc = ba.mul(PRECISION).div(bb)
+    const c = bc.toString()
+    if (c.length > 9)
+      return parseFloat(
+        c.substring(0, c.length - 9) +
+          '.' +
+          c.substring(c.length - 9, c.length),
+      )
+    else return parseFloat('0.' + '0'.repeat(9 - c.length) + c)
+  },
+}
+
+export default util
---
+++ test/utils.test.js
@@ -0,0 +1,14 @@
+const { utils } = require('../dist')
+const { payer } = require('./config')
+
+describe('Util library', function () {
+  it('Should create a consistent emoji', async function () {
+    const emoji = utils.randEmoji(payer.address)
+    if (emoji != '🐑') throw new Error('Inconsistent emoji')
+  })
+
+  it('Should parse a coingecko ticket', async function () {
+    const { name } = await utils.parseCGK('solana')
+    if (name != 'Solana') throw new Error('Cannot fetch Coingecko data')
+  })
+})
